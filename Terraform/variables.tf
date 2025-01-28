df12d9ee7b01fcc84f3fdff14495224c3f71f1d2
Roger Kristiansen <roger.kristiansen@norgesgruppen.no>
2025-01-28 12:57:53 +0100

    veriables change

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,23 +1,23 @@
-variable "subscription_id" {
-  description = "The Azure SP subscription ID"
-  type        = string
-  sensitive   = true
-}
-variable "client_id" {
-  description = "The Azure SP client ID"
-  type        = string
-  sensitive   = true
-}
-variable "client_secret" {
-  description = "The Azure SP client secret"
-  type        = string
-  sensitive   = true
-}
-variable "tenant_id" {
-  description = "The Azure SP tenant ID"
-  type        = string
-  sensitive   = true
-}
+# variable "subscription_id" {
+#   description = "The Azure SP subscription ID"
+#   type        = string
+#   sensitive   = true
+# }
+# variable "client_id" {
+#   description = "The Azure SP client ID"
+#   type        = string
+#   sensitive   = true
+# }
+# variable "client_secret" {
+#   description = "The Azure SP client secret"
+#   type        = string
+#   sensitive   = true
+# }
+# variable "tenant_id" {
+#   description = "The Azure SP tenant ID"
+#   type        = string
+#   sensitive   = true
+# }
 variable "domeneshop_api_token" {
   description = "The Domeneshop API token"
   type        = string
